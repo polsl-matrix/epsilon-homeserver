88017ecf53976205d225f890622798ce683744d1
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-03-25 18:05:12 +0100

    Configure CORS

--- src/Tesseract.Web/DependencyInjection.cs
+++ src/Tesseract.Web/DependencyInjection.cs
@@ -4,10 +4,22 @@ namespace Tesseract.Web;
 
 public static class DependencyInjection
 {
+    private static void DefaultCorsPolicy(CorsPolicyBuilder policy)
+    {
+        policy.AllowAnyOrigin();
+        policy.WithMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        policy.WithHeaders("X-Requested-With", "Content-Type", "Authorization");
+    }
+
     extension(IHostApplicationBuilder builder)
     {
         public void AddWebServices()
         {
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(DefaultCorsPolicy);
+            });
+
             builder.Services.AddControllers();
 
             builder.Services.AddOpenApi();
