18b6a58476fa4fe3b9f67331173bc5d4e0b29902
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-05-28 00:42:34 +0200

    Create (matrix id) domain value object

---
+++ src/Tesseract.Domain/Users/Values/Domain.cs
@@ -0,0 +1,36 @@
+using System.Text.RegularExpressions;
+using Tesseract.Domain.Common.Exceptions;
+
+namespace Tesseract.Domain.Users.Values;
+
+public sealed partial record Domain
+{
+    public Domain(string value)
+    {
+        var normalized = value.Trim();
+
+        if (!IsValidDomain(normalized))
+        {
+            throw new ValidationException("Invalid domain format.");
+        }
+
+        Value = normalized;
+    }
+
+    public string Value { get; }
+
+    // @formatter:off
+    [GeneratedRegex(@"^(?:(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(?:\[[0-9A-Fa-f:.]{2,45}\])|(?:[0-9A-Za-z-.]{1,255}))(?::\d{1,5})?$")]
+    private static partial Regex DomainRegex { get; }
+    // @formatter:on
+
+    private static bool IsValidDomain(string value) =>
+        DomainRegex.IsMatch(value);
+
+    public override string ToString() => Value;
+
+    public static implicit operator Domain(string value)
+    {
+        return new Domain(value);
+    }
+}
---
+++ tests/Tesseract.Domain.Tests/Users/DomainTests.cs
@@ -0,0 +1,88 @@
+using FluentAssertions;
+using Tesseract.Domain.Common.Exceptions;
+using VDomain = Tesseract.Domain.Users.Values.Domain;
+
+namespace Tesseract.Domain.Tests.Users;
+
+public class DomainTests
+{
+    [Fact]
+    public void Constructor_WithTrailingWhitespace_ShouldTrimValue()
+    {
+        const string value = "    test.thanks    ";
+        const string expected = "test.thanks";
+
+        var domain = new VDomain(value);
+
+        domain.Value.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("localhost")]
+    [InlineData("server.local:3901")]
+    [InlineData("192.168.15.15")]
+    [InlineData("[fe80::df89:d0c0]")]
+    [InlineData("grasshopper:3526")]
+    [InlineData("192.168.5.5:5754")]
+    [InlineData("[fe80::29fc:39f]:27930")]
+    public void Constructor_WithValidSequences_ShouldInitializeCorrectly(string value)
+    {
+        var domain = new VDomain(value);
+
+        domain.Value.Should().Be(value);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("local/host")]
+    [InlineData("server?local:3901")]
+    [InlineData("192.168.15.15:server")]
+    [InlineData("fe80::df89:d0c0")]
+    [InlineData("grasshopper:feed")]
+    [InlineData("fe80::29fc:39f:27930")]
+    [InlineData("[::]:390891")]
+    [InlineData("[fe80:111:1111:1111:1111:1111:1111:1111:1111:1111]:390891")]
+    public void Constructor_WithInvalidSequences_ShouldThrowValidationException(string value)
+    {
+        var act = () => new VDomain(value);
+
+        act.Should().Throw<ValidationException>();
+    }
+
+    [Theory]
+    [InlineData("localhost")]
+    [InlineData("epsilon.local:8374")]
+    [InlineData("127.0.0.1")]
+    [InlineData("[fe80::dd:3]:1892")]
+    public void ToString_Always_ShouldReturnActualValue(string value)
+    {
+        var domain = new VDomain(value);
+
+        var result = domain.ToString();
+
+        result.Should().Be(value);
+    }
+
+    [Theory]
+    [InlineData("172.16.81.30")]
+    [InlineData("[fe80::e232:d902]")]
+    [InlineData("rabbithole:3526")]
+    public void ImplicitOperator_FromValidValue_ShouldCreateLocalpart(string value)
+    {
+        VDomain domain = value;
+
+        domain.Should().NotBeNull();
+        domain.Value.Should().Be(value);
+    }
+
+    [Theory]
+    [InlineData("fe80::df89:d0c0")]
+    [InlineData("cabbage:carrot")]
+    public void ImplicitOperator_FromInValidValue_ShouldThrowValidationException(string value)
+    {
+        Func<VDomain> act = () => value;
+
+        act.Should().Throw<ValidationException>();
+    }
+}
