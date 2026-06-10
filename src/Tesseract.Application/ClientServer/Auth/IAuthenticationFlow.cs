b5cf2f5f1417bacf5558473419ad3745698f6244
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-06-11 01:14:23 +0200

    Create dummy authentication flow

---
+++ src/Tesseract.Application/ClientServer/Auth/IAuthenticationFlow.cs
@@ -0,0 +1,11 @@
+using Tesseract.Domain.Users.Entities;
+
+namespace Tesseract.Application.ClientServer.Auth.Abstractions;
+
+public interface IAuthenticationFlow
+{
+    string Type { get; }
+
+    Task<User?> AuthenticateAsync(string login, string password,
+        CancellationToken cancellationToken);
+}
