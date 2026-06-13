b818195505db29ca0a7383e2955514cc34ddd54a
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-06-13 18:36:19 +0200

    Create user mapper between dao and domain

---
+++ src/Tesseract.Infrastructure/ClientServer/Identity/Mappers/UserMapper.cs
@@ -0,0 +1,16 @@
+using Tesseract.Domain.Users;
+using Tesseract.Domain.Users.Values;
+using Tesseract.Infrastructure.ClientServer.Auth.Dao;
+
+namespace Tesseract.Infrastructure.ClientServer.Identity.Mappers;
+
+internal static class UserMapper
+{
+    public static User ToDomain(this UserDao dao)
+    {
+        var userId = new UserId(dao.UserId);
+        var handle = new UserHandle(dao.Localpart, dao.Domain);
+
+        return new User(userId, handle);
+    }
+}
---
+++ tests/Tesseract.Infrastructure.Tests/ClientServer/Identity/Mappers/UserMapperTests.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using Tesseract.Domain.Users;
+using Tesseract.Domain.Users.Values;
+using Tesseract.Infrastructure.ClientServer.Auth.Dao;
+using Tesseract.Infrastructure.ClientServer.Identity.Mappers;
+
+namespace Tesseract.Infrastructure.Tests.ClientServer.Identity.Mappers;
+
+public class UserMapperTests
+{
+    [Fact]
+    internal void ToDomain_ValidInput_ReturnsCorrectlyMappedUser()
+    {
+        var user = new User(
+            new UserId(Guid.NewGuid()),
+            new UserHandle("kangaroo", "jump.er")
+        );
+        var dao = new UserDao
+        {
+            UserId = user.Id.Value,
+            Localpart = user.Handle.Localpart.Value,
+            Domain = user.Handle.Domain.Value,
+        };
+
+        var result = dao.ToDomain();
+
+        result.Id.Value.Should().Be(user.Id.Value);
+        result.Handle.Localpart.Value.Should().Be(user.Handle.Localpart.Value);
+        result.Handle.Domain.Value.Should().Be(user.Handle.Domain.Value);
+    }
+}
