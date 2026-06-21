429dba10e2e0f9be4d5258de1a36ca26e5a740b4
LordPrinz <plazinski2902@gmail.com>
2026-06-21 20:25:22 +0200

    Fix

--- src/Tesseract.Web/Common/Observability/Telemetry.cs
+++ src/Tesseract.Web/Common/Observability/Telemetry.cs
@@ -1,21 +1,10 @@
 using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 namespace Tesseract.Web.Common.Observability;
 
 internal static class Telemetry
 {
-    private static void DefaultResource(IHostApplicationBuilder builder, ResourceBuilder resource)
-    {
-        var serviceName = Environment.GetEnvironmentVariable("OTEL_SERVICE_NAME");
-
-        resource.AddService(
-            serviceName: string.IsNullOrWhiteSpace(serviceName)
-                ? builder.Environment.ApplicationName
-                : serviceName);
-    }
-
     private static void DefaultRuntimeMetrics(MeterProviderBuilder options)
     {
         options.AddAspNetCoreInstrumentation()
@@ -40,7 +29,6 @@ internal static class Telemetry
         public void AddOpenTelemetry()
         {
             builder.Services.AddOpenTelemetry()
-                .ConfigureResource(options => DefaultResource(builder, options))
                 .WithMetrics(DefaultRuntimeMetrics)
                 .WithTracing(options => DefaultRuntimeTracing(builder, options));
         }
