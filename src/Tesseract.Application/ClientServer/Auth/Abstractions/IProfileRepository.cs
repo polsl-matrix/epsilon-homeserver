d3ea97a674fb671b738ac3e4f8b8d068df08c267
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-06-23 01:57:00 +0200

    Update existing endpoints to call a single use-case for retrieving profile details

--- src/Tesseract.Application/ClientServer/Auth/Abstractions/IProfileRepository.cs
+++ src/Tesseract.Application/ClientServer/Auth/Abstractions/IProfileRepository.cs
@@ -7,6 +7,5 @@ public interface IProfileRepository
     Task InsertAsync(Domain.Users.Profile profile, CancellationToken cancellationToken);
     Task UpsertDisplayNameAsync(UserId userId, string displayName, CancellationToken cancellationToken);
     Task UpsertAvatarUrlAsync(UserId userId, string avatarUrl, CancellationToken cancellationToken);
-    Task<string?> GetDisplayNameAsync(UserId userId, CancellationToken cancellationToken);
-    Task<string?> GetAvatarUrlAsync(UserId userId, CancellationToken cancellationToken);
+    Task<Domain.Users.Profile?> GetByUserIdAsync(UserId userId, CancellationToken cancellationToken);
 }
