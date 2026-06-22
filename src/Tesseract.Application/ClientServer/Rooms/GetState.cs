9243eaa407305512e6692340f67a5e82595cdbac
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-06-22 16:02:20 +0200

    [ES-68] Get state of a room ( /_matrix/client/v3/rooms/{roomId}/state) (#75)
    * Create repository method for querying state events
    
    * Create endpoint and usecase for getting state events in a room

---
+++ src/Tesseract.Application/ClientServer/Rooms/GetState.cs
@@ -0,0 +1,63 @@
+using MediatR;
+using Tesseract.Application.ClientServer.Auth.Exceptions;
+using Tesseract.Application.ClientServer.Identity.Abstractions;
+using Tesseract.Application.ClientServer.Rooms.Abstractions;
+using Tesseract.Application.ClientServer.Rooms.Exceptions;
+using Tesseract.Domain.Events;
+using Tesseract.Domain.Rooms;
+using Tesseract.Domain.Users;
+
+namespace Tesseract.Application.ClientServer.Rooms;
+
+public static class GetState
+{
+    public sealed record Query(string RoomHandle, UserId UserId) : IRequest<Response>;
+
+    internal sealed class Handler(
+        IEventRepository eventRepository,
+        IRoomRepository roomRepository,
+        IRoomMembershipRepository roomMembershipRepository,
+        IUserRepository userRepository)
+        : IRequestHandler<Query, Response>
+    {
+        public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+        {
+            if (await GetRoomByHandleAsync(request.RoomHandle, cancellationToken) is not { } room)
+            {
+                throw new RoomNotFoundException(request.RoomHandle);
+            }
+
+            if (await userRepository.GetByIdAsync(request.UserId, cancellationToken) is not { } sender)
+            {
+                throw new ForbiddenException();
+            }
+
+            if (await roomMembershipRepository.GetByRoomIdAndUserIdAsync(room.Id, sender.Id, cancellationToken) is null)
+            {
+                throw new UserNotInRoomException();
+            }
+
+            var events = await eventRepository
+                .GetStateByRoomIdAsync(EventTypes.MemberRoom, room.Id, cancellationToken);
+
+            return new Response(events.ToArray());
+        }
+
+        private async Task<Room?> GetRoomByHandleAsync(string handle, CancellationToken cancellationToken)
+        {
+            if (!RoomHandle.TryParse(handle, out var roomHandle))
+            {
+                return null;
+            }
+
+            if (await roomRepository.GetByHandleAsync(roomHandle, cancellationToken) is not { } room)
+            {
+                return null;
+            }
+
+            return room;
+        }
+    }
+
+    public sealed record Response(IReadOnlyList<string> Events);
+}
--- src/Tesseract.Infrastructure/ClientServer/Events/DbEventRepository.cs
+++ src/Tesseract.Infrastructure/ClientServer/Events/DbEventRepository.cs
@@ -83,6 +83,28 @@ internal class DbEventRepository : IEventRepository
         return await connection.QueryAsync<string>(sql, parameters);
     }
 
+    public async Task<IEnumerable<string>> GetStateByRoomIdAsync(string type, RoomId roomId,
+        CancellationToken cancellationToken)
+    {
+        await using var connection = _dbConnectionFactory.CreateConnection();
+
+        const string sql = """
+                           SELECT payload
+                           FROM chat.room_events
+                           WHERE room_id = @RoomId
+                             AND state_key IS NOT NULL 
+                           ORDER BY timestamp;
+                           """;
+
+        var parameters = new
+        {
+            EventType = type,
+            RoomId = roomId.Value,
+        };
+
+        return await connection.QueryAsync<string>(sql, parameters);
+    }
+
     private string SerializePayload(Event @event)
     {
         if (!_eventMappers.TryGetValue(@event.Type, out var eventMapper))
